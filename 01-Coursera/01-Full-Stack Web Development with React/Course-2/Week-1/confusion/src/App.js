d22b3a295cc80d3f7ae4dd95efd3f7e61a8a51e8
karankumarjs <karankmr.dev@gmail.com>
2022-02-05 02:13:13 +0530

    completed

---
+++ 01-Coursera/01-Full-Stack Web Development with React/Course-2/Week-1/confusion/src/App.js
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
+
+
+
+function App() {
+ 
+ 
+  const [dishes, setdishes] = useState(DISHES);
+  return (
+    <div className="App">
+      <Navbar dark color="primary">
+        <div className="container">
+          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+        </div>
+      </Navbar>
+      <Menu dishes={dishes} />
+    </div>
+  );
+}
+
+export default App;
---
+++ 01-Coursera/01-Full-Stack Web Development with React/Course-2/Week-1/confusion/src/components/Dishdetail.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+
+import { Card, CardImg, CardImgOverlay, CardText, CardBody,CardTitle } from 'reactstrap';
+
+
+class DishDetail extends Component {
+
+/* ----------------------dish-image-name---------------------- */
+    renderDish(dish){
+
+        if(dish!=null){
+            return(
+                <div className='col-12 col-md-5 m-1'>
+                    <Card>
+                        <CardImg width="100%" src={dish.image} alt={dish.name} />
+                        <CardBody>
+                            <CardTitle> {dish.name}</CardTitle>
+                            <CardText> {dish.description} </CardText>
+                        </CardBody>
+                    </Card>
+                </div>   
+            )
+
+        }
+        else{
+            return(<div></div>);
+        }
+        
+    }
+/* ----------------------dish-comments---------------------- */
+
+    renderComments(comments){
+        if (comments==null) {
+            return (<div></div>)
+        }    
+        const cmnts=comments.map(comment=>{
+            return(
+                <li key={comment.id}>
+                    <p>{comment.comment}</p>
+                    <p>{comment.author}</p>
+                </li>
+            )
+        })
+        return(
+            <div className='col-12 col-md-5 m-1'>
+            <h4> Comments </h4>
+            <ul className='list-unstyled'>
+                {cmnts}
+            </ul>
+
+        </div>
+        )
+
+    }
+
+    render(){
+        const dish=this.props.dish;
+        if (dish==null) {
+            return(<div></div>);
+        }
+        const dishItem=this.renderDish(dish);
+        const dishComment=this.renderComments(dish.comments);
+
+        return(
+            <div className="row">
+            {dishItem}
+            {dishComment}
+            </div>
+        )
+    }
+}
+
+
+export default DishDetail
